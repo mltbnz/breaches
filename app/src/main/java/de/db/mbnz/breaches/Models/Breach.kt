e6c6fbbc41135b3fa49047973cfd89f5bd814bd8
Malte Bünz <malte.buenz@deutschebahn.com>
2017-11-16 22:10:26 +0100

    using data class

--- app/src/main/java/de/db/mbnz/breaches/Models/Breach.kt
+++ app/src/main/java/de/db/mbnz/breaches/Models/Breach.kt
@@ -1,5 +1,6 @@
 package de.db.mbnz.breaches.Models
 
+import android.os.Parcelable
 import java.io.Serializable
 import java.util.*
 
@@ -7,24 +8,22 @@ import java.util.*
  * Created by mbnz on 08.11.17.
  */
 
-class Breach(val Name: String,
-             val Title: String,
-             val Domain: String,
-             val BreachDate: String,
-             val AddedDate: String,
-             val ModifiedDate: String,
-             val PwnCount: Int,
-             val Description: String,
-             val DataClasses: Array<String>,
-             val IsVerified: Boolean,
-             val OsFabricated: Boolean,
-             val IsSensitive: Boolean,
-             val IsActive: Boolean,
-             val IsRetired: Boolean,
-             val IsSpamList: Boolean): Serializable {
-
-    init {
-    }
+data class Breach(
+        val Name: String,
+        val Title: String,
+        val Domain: String,
+        val BreachDate: String,
+        val AddedDate: String,
+        val ModifiedDate: String,
+        val PwnCount: Int,
+        val Description: String,
+        val DataClasses: Array<String>,
+        val IsVerified: Boolean,
+        val OsFabricated: Boolean,
+        val IsSensitive: Boolean,
+        val IsActive: Boolean,
+        val IsRetired: Boolean,
+        val IsSpamList: Boolean): Serializable {
 
     companion object Keys {
         val titleKey = "Title"
